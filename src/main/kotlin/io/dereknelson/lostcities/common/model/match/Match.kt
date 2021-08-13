b9577fbf8ac73c076c5170229b2c8eeb211ce2f5
Derek <contact@dereknelson.io>
2021-08-13 19:45:31 -0400

    Updates to match controller.

--- src/main/kotlin/io/dereknelson/lostcities/common/model/match/Match.kt
+++ src/main/kotlin/io/dereknelson/lostcities/common/model/match/Match.kt
@@ -2,21 +2,27 @@ package io.dereknelson.lostcities.common.model.match
 
 import io.dereknelson.lostcities.common.model.User
 import java.time.LocalDateTime
+import java.util.*
 
 
-data class Match (
-    val id: Long,
+open class Match (
+    val id: Long? = null,
     val players: UserPair,
     val seed: Long,
-
     val isReady: Boolean = false,
     val isStarted: Boolean = false,
     val isCompleted: Boolean = false,
 
-    val concededBy: Long?,
+    val concededBy: Long? = null,
 
-    val createdDate: LocalDateTime,
-    val lastModifiedDate: LocalDateTime,
-    val createdBy: String,
+    val createdDate: LocalDateTime? = null,
+    val lastModifiedDate: LocalDateTime? = null,
+    val createdBy: String? = null,
     var lastModifiedBy: String? = null
-)
+) {
+    companion object {
+        fun buildMatch(player: Long, random: Random): Match{
+            return Match(players= UserPair(user1=player), seed=random.nextLong())
+        }
+    }
+}
