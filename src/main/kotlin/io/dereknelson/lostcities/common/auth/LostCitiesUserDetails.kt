11951002505699eecf77a9a43c243c6d746a13ed
nelson54 <me@dereknelson.io>
2024-11-16 18:56:52 -0500

    Fixing null pointer issue in command.

--- src/main/kotlin/io/dereknelson/lostcities/common/auth/LostCitiesUserDetails.kt
+++ src/main/kotlin/io/dereknelson/lostcities/common/auth/LostCitiesUserDetails.kt
@@ -4,7 +4,7 @@ import io.dereknelson.lostcities.common.auth.entity.UserRef
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 
-class LostCitiesUserDetails(
+open class LostCitiesUserDetails(
     val id: Long,
     val login: String,
     val email: String,
